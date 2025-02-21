130950396e3db1c63371dde7d37914c3dfd15b6d
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-21 15:48:39 -0500

    Add for...else

--- control_flow.py
+++ control_flow.py
@@ -110,3 +110,14 @@ for number in range(1, 4):
 
 for number in range(1, 10, 2):
     print("Attempt", number, (number) * ".")
+
+#############################################################
+# For...Else
+successful = False
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful")
+        break
+else:
+    print("Attempted 3 times and failed")
