a5872cb7a8f7144b287ee16f4af27ed4c0ffcb66
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-21 16:04:52 -0500

    Add iterables

--- control_flow.py
+++ control_flow.py
@@ -127,3 +127,21 @@ else:
 for x in range(5):
     for y in range(3):
         print(f"({x}, {y})")
+
+#############################################################
+# Iterables
+print(type(5))
+print(type(range(5)))
+
+# Iterable
+for x in range(5):
+    print(x)
+
+for x in "Python":
+    print(x)
+
+for x in [1, 2, 3, 4]:
+    print(x)
+
+# for item in shopping_cart:
+#     print(item)
