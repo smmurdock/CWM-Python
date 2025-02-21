4e26e3bf34d8fab98207695066d206b670d7e5b6
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-21 15:45:17 -0500

    Add for loops

--- control_flow.py
+++ control_flow.py
@@ -97,3 +97,16 @@ if age >= 18 and age < 65:
 
 if 18 <= age < 65:
     print("Eligible")
+
+#############################################################
+# For Loops
+print("Sending a message")
+for number in range(3):
+    print("Attempt", number + 1, (number + 1) * ".")
+
+for number in range(1, 4):
+    print("Attempt", number, (number) * ".")
+
+
+for number in range(1, 10, 2):
+    print("Attempt", number, (number) * ".")
