a307104bae300319a1c93be37bab0dd091d616bc
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-21 13:04:21 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+# Variables
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Python Programming"
+print(students_count)
+
+# Variable Names
+# 1. Use descriptive names
+# 2. Use lowercase letters
+# 3. Use underscore to separate words (snake_case)
+# 4. Use spaces around equal signs
+
+# Strings
+course = "Python Programming"
+print(len(course))
+print(course[0])
+print(course[-1])
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
+print(course[1:-1])
