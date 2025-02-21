03448aedeb6ca0c621d74de32ed4d5312ae9b6b7
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-21 15:18:00 -0500

    Add ternary operator

--- control_flow.py
+++ control_flow.py
@@ -23,3 +23,23 @@ elif temperature > 20:
 else:
     print("It's cold")
 print("Done")
+
+#############################################################
+# Ternary Operator
+age = 22
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not eligible")
+
+# Achieve the same result with different syntax
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not eligible"
+print(message)
+
+# Achieve the same result with a ternary operator
+age = 12
+message = "Eligible" if age >= 18 else "Not eligible"
+print(message)
