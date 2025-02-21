aafb799eefb735f6da7e321165dcefa2b220ef6c
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-21 15:14:48 -0500

    Add conditional statements

--- control_flow.py
+++ control_flow.py
@@ -11,3 +11,15 @@ print("bag" > "apple")
 print("bag" == "BAG")
 print(ord("b"))
 print(ord("B"))
+
+#############################################################
+# Conditional Statements
+temperature = 15
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
