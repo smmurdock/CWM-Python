68ed0e678df6797a5257f08e6e4647055d40f9ec
Shanay Murdock <murdock.shanay@gmail.com>
2025-02-21 13:24:36 -0500

    Add string methods

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#############################################################
 # Variables
 students_count = 1000
 rating = 4.99
@@ -5,12 +6,14 @@ is_published = False
 course_name = "Python Programming"
 print(students_count)
 
+#############################################################
 # Variable Names
 # 1. Use descriptive names
 # 2. Use lowercase letters
 # 3. Use underscore to separate words (snake_case)
 # 4. Use spaces around equal signs
 
+#############################################################
 # Strings
 course = "Python Programming"
 print(len(course))
@@ -34,6 +37,7 @@ print(course)
 # \\
 # \n
 
+#############################################################
 # Formatted Strings
 first = "Shanay"
 last = "Murdock"
@@ -41,3 +45,25 @@ full = first + " " + last
 print(full)
 full = f"{first} {last}"
 print(full)
+
+#############################################################
+# String Methods
+course = "Python Programming"
+print(course.upper())
+course_upper = course.upper()
+print(course)
+print(course.lower())
+
+course = "python programming"
+print(course.title())
+
+course = "    Python Programming  "
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
+
+course = "Python Programming"
+print(course.find("Pro"))  # returns index; -1 means not found
+print(course.replace("P", "-"))
+print("Pro" in course)  # returns boolean
+print("swift" not in course)
